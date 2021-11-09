9d22fac896438d4d0bfc576e129cf461447bff0b
Luann Rodrigo <luann.rodrigos@gmail.com>
2021-11-09 10:55:05 -0300

    add pages (HOME, CONTACT and FEEDBAKCS)

---
+++ src/views/Home/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <Header
+    @create-account="AccountCreate"
+    @login="Login"
+  />
+  <Contact />
+  <footer class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">Feedbacker & 2021</p>
+  </footer>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+import Header from '../../components/Header';
+import Contact from '../../components/Contact';
+
+export default {
+  components: {
+    Header,
+    Contact,
+  },
+
+  setup() {
+    const router = useRouter();
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token');
+
+      if (token) {
+        // this.$router.push('/dashboard');
+        router.push({ name: 'Feedbacks' });
+      }
+    });
+    function Login() {
+
+    }
+
+    function AccountCreate() {
+
+    }
+
+    return {
+      Login,
+      AccountCreate,
+    };
+  },
+
+};
+</script>
+
+<style>
+
+</style>
