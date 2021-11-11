e3bd76308122c06f587b0cfa1511b7c9cba0bd6d
Luann Rodrigo <luann.rodrigos@gmail.com>
2021-11-11 11:11:52 -0300

    create functions and actions to mount em ummount modal

--- src/components/Modal/index.vue
+++ src/components/Modal/index.vue
@@ -5,42 +5,71 @@
       class="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50"
       @click="ModalToogle({ status: false })"
     >
-      <div class="fixed mx-10">
-        <div class="flex flex-col overflow-hidden bg-white rounded-lg animete__animarted animate__fadeInDown animate__falster">
+      <div
+        class="fixed mx-10"
+        :class="state.width"
+        @click.stop
+      >
+        <div class="flex flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeInDown animate__faster">
           <div class="flex flex-col px-12 py-10 bg-white">
-            <component :is="state.content"/>
+            <component :is="state.component" />
           </div>
         </div>
+
       </div>
     </div>
   </teleport>
 </template>
 
 <script>
-import { reactive } from 'vue';
+import {
+  reactive, onMounted, onBeforeUnmount, defineAsyncComponent,
+} from 'vue';
+import useModal from '../../hooks/useModal';
+
+const ModalLogin = defineAsyncComponent(() => import('./ModalLogin'));
 
 const DEFAULT_WIDTH = 'w-3/4 lg:w-1/3';
 
 export default {
+  components: {
+    ModalLogin,
+  },
   setup() {
+    const modal = useModal();
     const state = reactive({
       isActive: false,
-      content: {},
+      component: {},
       props: {},
-      with: DEFAULT_WIDTH,
+      width: DEFAULT_WIDTH,
     });
 
-    function ModalToogle({ status }) {
+    function ModalToogle(payload) {
+      if (payload.status) {
+        state.component = payload.component;
+        state.props = payload.props;
+        state.width = payload.width ?? DEFAULT_WIDTH;
+      } else {
+        state.component = {};
+        state.props = {};
+        state.width = DEFAULT_WIDTH;
+      }
 
+      state.isActive = payload.status;
     }
+
+    onMounted(() => {
+      modal.listen(ModalToogle);
+    });
+
+    onBeforeUnmount(() => {
+      modal.off(ModalToogle);
+    });
 
     return {
       state,
+      ModalToogle,
     };
   },
 };
 </script>
-
-<style>
-
-</style>
