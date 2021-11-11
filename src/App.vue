56be4e25db22bcf87ea984c5dc19aef5ae3cfc7b
Luann Rodrigo <luann.rodrigos@gmail.com>
2021-11-11 11:10:23 -0300

    add import modal in App.vue to use Modal all pages

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
+  <modal />
   <router-view/>
 </template>
+
+<script>
+import Modal from './components/Modal';
+
+export default {
+  components: {
+    Modal,
+  },
+};
+</script>
 
 <style>
 
