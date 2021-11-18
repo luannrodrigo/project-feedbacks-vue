45cbcbc718932191a6a6412464a2257260a294dd
Luann Rodrigo <luann.rodrigos@gmail.com>
2021-11-18 17:48:56 -0300

    method state global to current user

---
+++ src/store/user.js
@@ -0,0 +1,22 @@
+import { reactive } from 'vue';
+
+const state = reactive({
+  currentUser: {
+
+  },
+});
+
+export default state;
+
+export const clearCurrentUser = () => {
+  state.currentUser = {};
+};
+
+export const setCurrentUser = (user) => {
+  state.currentUser = user;
+};
+
+export const setApiKey = (apiKey) => {
+  const currentUser = { ...state.currentUser, apiKey };
+  state.currentUser = currentUser;
+};
